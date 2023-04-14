65cf4f9f53a7cd31ccd035d649387765f79ff52a
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-04-14 12:30:35 +0530

    Create EventBind.tsx

---
+++ react-app-2023-apr/src/event-handling/EventBind.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class EventBind extends Component {
+  render() {
+    return (
+      <div>
+        <button  >Click Me To Load</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind
